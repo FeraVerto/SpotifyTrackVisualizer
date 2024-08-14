60cda026f82ce651f9240112e2ec36497536d6d2
FeraVerto <themightymasha@gmail.com>
2024-08-14 23:45:15 +0400

    Получение данных из csv и отправка на фронт

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var csv = require('csv-parser');
+const fs = require('fs');
+const path = require('path');
 
 router.get('/data', function (req, res, next) {
-  const data = { message: 'Hello from the backend!' };
-  res.send(data);
+  const results = [];
+  const filePath = path.join(
+    __dirname,
+    '../files/MostStreamedSpotifySongs2024.csv'
+  );
+  fs.createReadStream(filePath)
+    .pipe(csv({ separator: '\t' }))
+    .on('data', (data) => {
+      results.push(data);
+    })
+    .on('end', () => {
+      res.json(results);
+    })
+    .on('error', (err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
